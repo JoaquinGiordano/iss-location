dcb9796294a0ce3ae70a84e5e4f252ca043bc7f6
Joaquin Giordano <joaquin_giordano@outlook.com>
2020-05-04 16:22:39 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			long: '',
+			lat: ''
+		}
+	}
+
+	componentDidMount() {
+		fetch('http://api.open-notify.org/iss-now.json')
+			.then((data) => {
+				return data.text()
+			})
+			.then((data) => {
+				return JSON.parse(data)
+			})
+			.then((myJson) => {
+				this.setState({
+					long: Number(myJson.iss_position.longitude).toFixed(2),
+					lat: Number(myJson.iss_position.latitude).toFixed(2)
+				})
+			})
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<h2>Longitude: {this.state.long}°</h2>
+				<h2>Latitude: {this.state.lat}°</h2>
+			</React.Fragment>
+		)
+	}
 }
 
-export default App;
+export default App
