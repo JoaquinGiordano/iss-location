d24f7b945ac54d2029ede34d6c78b7c11ec0b0d7
Joaquin Giordano <joaquin_giordano@outlook.com>
2020-05-04 17:27:58 -0300

    Updates

---
+++ precache-manifest.400e28319c4afed439fdcde9494aa084.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d6b75dc9ef98e9975bbd6123318266",
+    "url": "/iss-location/index.html"
+  },
+  {
+    "revision": "fa9b8876fda6cf962759",
+    "url": "/iss-location/static/css/main.e61b19b7.chunk.css"
+  },
+  {
+    "revision": "b66f1f400dced5b074ab",
+    "url": "/iss-location/static/js/2.7030293a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iss-location/static/js/2.7030293a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa9b8876fda6cf962759",
+    "url": "/iss-location/static/js/main.27aa6490.chunk.js"
+  },
+  {
+    "revision": "fedb17048f08437d7be4",
+    "url": "/iss-location/static/js/runtime-main.5b1c0efa.js"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/iss-location/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/iss-location/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/iss-location/static/media/Montserrat-Thin.43dd5b7a.ttf"
+  }
+]);
